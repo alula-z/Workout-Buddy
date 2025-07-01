50110772b060021e9ed80e9b2764c416c891f03c
alula-z <azeruesenay14@gmail.com>
2025-06-30 20:53:48 -0400

    Created initial state of data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,58 @@
 import Layout from '../layouts/layouts'
-import {auth }from '../firebase';
+import {auth, db }from '../firebase';
 import { useState } from 'react';
 import { PieChart} from '@mui/x-charts/PieChart';
+import { collection, getDocs } from 'firebase/firestore';
 export default function Home() {
+    type basketballDataForm = {
+        date: string,
+        madeShots: number,
+        madeThrees: number,
+        missedShots: number,
+        missedThrees: number,
+        totalShots: number,
+        totalThreeAttempted: number,   
+    }
+    type runningDataForm = {
+        average_time: string,
+        date: '',
+        distance_ran_mi: number,
+        time_ran: string,
+    }
 
+    const initialRunningData : runningDataForm = {
+        average_time: '',
+        date: '',
+        distance_ran_mi: 0,
+        time_ran: '',
+    }
+    const initialBasketballData : basketballDataForm = {
+        date:'',
+        madeShots: 0,
+        madeThrees: 0,
+        missedShots: 0,
+        missedThrees: 0,
+        totalShots: 0,
+        totalThreeAttempted: 0,
+    }
+    
     const user = auth.currentUser;
     const [sport, setSport] = useState('');
+    const [basketballData, setBasketballData] = useState(initialBasketballData);
+    const [runningData, setRunningData] = useState(initialRunningData);
+    const fetchCollection= async() =>{
+        try{
+            if(user){
+                const fetchedCollection = collection(db, "users", user.uid, sport);
+                const fetchedDocs = getDocs(fetchedCollection);
+                (await fetchedDocs).forEach((doc) => {
+                    d
+                })
+            }
+        }catch(error){
+            console.log(error);
+        }
+    }
     return (
     <Layout>
         <h1> Welcome {user?.email}</h1>
